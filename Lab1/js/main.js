f4cb47ef1504702e8b3d25fd9bf4a16b749fb5da
VilenSolovey <solovey.vilen21@gmail.com>
2024-09-17 21:29:08 +0300

    Lab2 changes

--- Lab1/js/main.js
+++ Lab1/js/main.js
@@ -1,7 +1,16 @@
-const OPEN_CLASSNAME ='open'
+const OPEN_CLASSNAME = 'open';
 
-const navLinks = document.getElementById("nav-links")
+const navLinks = document.getElementById("nav-links");
+const menuIcon = document.getElementById("menu-icon");
 
 function toggleMenu() {
-    navLinks.classList.toggle(OPEN_CLASSNAME)
+    navLinks.classList.toggle(OPEN_CLASSNAME);
+
+
+    if (navLinks.classList.contains(OPEN_CLASSNAME)) {
+        menuIcon.src = '/Users/vilen/Desktop/Web-Technologies/Lab1/assets/closee.png';
+    } else {
+        menuIcon.src = '/Users/vilen/Desktop/Web-Technologies/Lab1/assets/navi.svg';
+    }
 }
+
